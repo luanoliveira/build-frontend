055f746e4d5eb058bc76a57931884e2a952846a2
Luan Oliveira <luanconecte@gmail.com>
2017-10-08 15:40:04 -0300

    container de list component

--- src/components.js
+++ src/components.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import DeviceContainer from './components/DeviceContainer.vue';
 import Device from './components/Device.vue';
 
+Vue.component('device-container', DeviceContainer);
 Vue.component('device', Device);
---
+++ src/components/DeviceContainer.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="devices" v-if="devices">
+		<device 
+			:name="device.name"
+			:icon="device.icon"
+			v-for="device in devices"></device>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		props: {
+			devices: Array
+		},
+		data: function() {
+			return {};
+		}
+	};
+</script>
