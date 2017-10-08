73a133fd42dd6433b00c76ab1d2a7a22f01c0e83
Luan Oliveira <luanconecte@gmail.com>
2017-10-08 14:38:44 -0300

    corrige fluxo components vue

--- src/components.js
+++ src/components.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 
 import Post from './components/Post.vue';
 import Button from './components/Button.vue';
+import Device from './components/Device.vue';
 
 Vue.component('post', Post);
 Vue.component('my-button', Button);
+
+Vue.component('device', Device);
---
+++ src/components/Device.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="device">
+		<span class="device-name">{{ name }}</span>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		props: {
+			name: String
+		},
+		data: function() {
+			return {};
+		}
+	};
+</script>
+
+<style lang="scss">
+	.device {
+		border: 1px solid red;
+		padding: 15px;
+		margin-bottom: 15px;
+		border-radius: 2px;
+	}
+</style>
